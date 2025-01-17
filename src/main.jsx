251d4a1cdf49b2b095f587c13e53e48da55dd201
MATP1709 <102614204+MATP1709@users.noreply.github.com>
2025-01-17 16:42:34 -0500

    configuración de vite

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,8 @@ import { Body } from './components/Body/Body'
 import { Footer } from './components/Footer/Footer'
 import Aos from 'aos'
 import 'aos/dist/aos.css'; // Importa los estilos de AOS
-
 import './style.css'
+
 Aos.init({
   duration: 1200, // Puedes personalizar el tiempo de duración de las animaciones
   once: true, // Hacer que la animación se ejecute solo una vez
